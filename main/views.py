d5ed4af1af9788bb008640a59fcffe39a4eecff9
ghina2703 <ghina2703@gmail.com>
2023-09-09 23:31:25 +0700

    Membuat dan mengisi file main.html

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'app_name': 'Restaurant Stock Management',
-        'name': 'Ghina Nabila Gunawan',
-        'class': 'PBP B'
+        'app_name': 'ghina2703',
+        'my_name': 'Ghina Nabila Gunawan',
+        'my_class': 'PBP B'
     }
     return render(request, "main.html", context)

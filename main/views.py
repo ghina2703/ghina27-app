e4b2349db14bbe1bde6ce2636cb1b9f7e70802d7
ghina2703 <ghina2703@gmail.com>
2023-09-09 21:44:45 +0700

    Membuat sebuah fungsi pada views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'app_name': 'Restaurant Stock Management',
+        'name': 'Ghina Nabila Gunawan',
+        'class': 'PBP B'
+    }
+    return render(request, "main.html", context)

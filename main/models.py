1eaddab289f68ac06d3676f5f4c326ca0e13d655
ghina2703 <ghina2703@gmail.com>
2023-09-10 20:25:01 +0700

    Menambahkan models 'price'

--- main/models.py
+++ main/models.py
@@ -5,3 +5,4 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    price = models.IntegerField(default = 30000)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
 def show_main(request):
     context = {
         'app_name': 'ghina2703',
         'my_name': 'Ghina Nabila Gunawan',
-        'my_class': 'PBP B'
+        'my_class': 'PBP B',
+        'item': Item.objects.all(),
     }
     return render(request, "main.html", context)
